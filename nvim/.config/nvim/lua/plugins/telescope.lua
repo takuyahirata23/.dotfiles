274a7ab07cc704714b553d9a8b0bfb7092347827
Takuya Hirata <takuyahirata4@gmail.com>
2023-12-20 22:34:27 -0500

    Config: serch all the files in repo with telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    vim.keymap.set('n', '<C-e>', builtin.git_files, {})
     vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
     vim.keymap.set('n', 'lf', builtin.buffers, {})
     vim.keymap.set('n', 'gb', builtin.git_branches, {})
