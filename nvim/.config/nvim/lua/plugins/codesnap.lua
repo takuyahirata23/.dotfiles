976e8cb0ef49ded1b2e9b948d944085d7939beb4
Takuya Hirata <takuyahirata4@gmail.com>
2025-01-09 07:31:04 -0500

    Config: hide watermark on codesnap

--- nvim/.config/nvim/lua/plugins/codesnap.lua
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -2,11 +2,12 @@ return {
   "mistricky/codesnap.nvim",
   build = "make build_generator",
   keys = {
-    { "<leader>ts", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Pictures" },
+    { "<leader>ts", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Desktop" },
   },
   opts = {
     save_path = "~/Desktop",
     has_breadcrumbs = false,
     bg_theme = "sea",
+    watermark = ""
   },
 }
