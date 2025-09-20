361047478094f3b5f749a8e52d1f2fe836f3183c
Takuya Hirata <takuyahirata4@gmail.com>
2025-09-19 22:00:39 -0400

    Config: gitsigns and copilot

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup({})
+    require("gitsigns").setup({
+      current_line_blame = true
+    })
+
+    vim.keymap.set("n", "<Leader>tb", ":Gitsigns toggle_current_line_blame<CR>", {})
   end
 }
