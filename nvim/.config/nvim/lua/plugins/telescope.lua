6f560e339bf29a63aff0b4150846a2101387bf7f
Takuya Hirata <takuyahirata4@gmail.com>
2025-10-17 08:08:21 -0400

    Config: telescope to use recent files instead of harpoon

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,36 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
+    local actions = require('telescope.actions')
 
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<C-e>', builtin.git_files, {})
+    vim.keymap.set('n', '<C-e>', function()
+      builtin.find_files({
+        hidden = true,
+        no_ignore = true,
+        find_command = {
+          'fd', '--type', 'f', '--hidden', '--no-ignore', '--exclude', '.git', '--exclude', 'node_modules', '--exclude',
+          'dist', '--exclude', 'build', '--exclude', '.elixir-ls', '--exclude', '.dart_tool', '--exclude', 'data',
+          '--exclude', 'deps'
+        }
+      })
+    end)
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-    vim.keymap.set('n', 'lf', builtin.buffers, {})
+    vim.keymap.set('n', 'rf', function()
+      require('telescope.builtin').buffers({
+        ignore_current_buffer = true,
+        sort_lastused = true,
+        show_all_buffers = true,
+        attach_mappings = function(_, map)
+          map('i', '<C-d>', actions.delete_buffer)
+          map('n', '<C-d>', actions.delete_buffer)
+          return true
+        end,
+      })
+    end, {})
     vim.keymap.set('n', 'gb', builtin.git_branches, {})
   end
 }
