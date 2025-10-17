10992797b14ed12b304f0b6b4619ad34ca22c9b5
Takuya Hirata <takuyahirata4@gmail.com>
2025-10-17 07:37:25 -0400

    Config: remove git fugitive

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,6 +1,7 @@
-return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set('n', '<Leader>gs', vim.cmd.Git)
-  end
-}
+-- return {
+--   "tpope/vim-fugitive",
+--   config = function()
+--     vim.keymap.set('n', '<Leader>gs', vim.cmd.Git)
+--   end
+-- }
+return {}
